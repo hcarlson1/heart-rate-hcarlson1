7c5baf7f67f7c610b75ee164a8e35258a7df1c3b
hcarlson1 <hcarlson1@css.edu>
2017-02-17 11:36:35 -0600

    class changes

---
+++ app/src/main/java/css/cis3334/heartratetracker/HeartRateAdapter.java
@@ -0,0 +1,47 @@
+package css.cis3334.heartratetracker;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by hcarlson1 on 2/17/2017.
+ */
+
+public class HeartRateAdapter extends ArrayAdapter<HeartRate> {
+    private Context context;
+    private HeartRateList heartRateList;
+
+    public HeartRateAdapter(Context context,
+                            int resource,
+                            int textViewResourceId,
+                            HeartRateList heartRateList) {
+        super(context, resource, textViewResourceId, heartRateList.getList());
+
+        this.context = context;
+        this.heartRateList = heartRateList;
+
+
+    }
+    public View getView(int position, View convertView, ViewGroup parent){
+
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.heart_rate_row, null);
+        //get the heart rate we are displaying
+        TextView tvPulse = (TextView) view.findViewById(R.id.textViewPulse);
+        HeartRate hr = heartRateList.getHeartRate(position);
+
+        tvPulse.setText(hr.getPulse().toString());
+
+
+        return(view);
+
+    }
+}
--- app/src/main/java/css/cis3334/heartratetracker/MainActivity.java
+++ app/src/main/java/css/cis3334/heartratetracker/MainActivity.java
@@ -3,16 +3,43 @@ package css.cis3334.heartratetracker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
 
     HeartRateList heartRateList;        // The list of heart rate objects
+    ListView lvHeartRate;
+    TextView tvSelect;
+    ArrayAdapter<HeartRate> hrAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lvHeartRate = (ListView)findViewById(R.id.listViewHeartRate);
+        tvSelect = (TextView)findViewById(R.id.textViewHeartRate);
+
         heartRateList = new HeartRateList();
-        heartRateList.InitRandomElderly();
+        heartRateList.InitRandomYouth();
+
+        //ArrayAdapter<HeartRate> planetAdapter = new ArrayAdapter<HeartRate>(this, android.R.layout.simple_list_item_1, basicheartRateList);
+        hrAdapter = new HeartRateAdapter(this, R.layout.heart_rate_row, R.id.textViewPulse, heartRateList);
+        hrAdapter.setDropDownViewResource(R.layout.heart_rate_row);
+        lvHeartRate.setAdapter(hrAdapter);
+
+        lvHeartRate.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                HeartRate hr = (HeartRate) parent.getItemAtPosition(position);
+                tvSelect.setText("You selected: " + hr.toString());
+            }
+        });
     }
 }
